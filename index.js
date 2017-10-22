74e6022c3b774a3c3a76576cc2ddb872f247de3a
Katy Carr <khcarr9@gmail.com>
2017-10-22 17:26:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let increaseRankBy = function(n) {
 let deepestChild = function() {
   let grandNode = document.querySelector('#grand-node');
   let child = grandNode.children[0];
-  while(child.children != 0) {
+  while(child.children) {
     child = child.children[0];
   }
   return child.innerHTML;
