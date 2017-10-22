12ecdaa6ae10ad0a7972d00673f23e3a2d2e8a97
Katy Carr <khcarr9@gmail.com>
2017-10-22 16:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var getFirstSelector = function(selector) {
   var result = document.querySelector(selector);
   return result;
 }
+
+var nestedTarget() {
+  var nested = document.querySelector('#nested');
+  return nested.querySelector('.target');
+}
